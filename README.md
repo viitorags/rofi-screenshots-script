fc605b869e10e684ea07dfa30b20c55da6b9f8de
viitorags <vitorgabriels1000@gmail.com>
2025-06-04 19:23:20 -0300

    Adding files

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# rofi-screnshots-script
+# Screenshot & Screen Recording Tool
+
+This is a Bash script for taking screenshots and recording videos using Rofi as a graphical interface on Linux systems with Wayland (specifically for Hyprland).
+
+## Features
+
+- Fullscreen screenshot  
+- Select area screenshot  
+- Active window screenshot  
+- Delayed screenshot (5 seconds)  
+- Fullscreen screen recording  
+- Select area screen recording  
+- Stop ongoing recording  
+
+## Dependencies
+
+- rofi  
+- grim (for taking screenshots)  
+- slurp (for selecting areas)  
+- wf-recorder (for screen recording)  
+- jq (for handling JSON)  
+- notify-send (for notifications)  
+
+## Installation
+
+1. Clone this repository:  
+```bash
+git clone https://github.com/viitorags/rofi-screenshots-script.git
+```
+
+2. Make the script executable:
+
+```bash
+chmod +x screenshot.sh
+```
+
+3. Copy the theme file to the Rofi configuration directory:
+
+```bash
+cp screenshot.rasi ~/.config/rofi/
+```
+
+## Usage
+
+Run the script:
+
+```bash
+./screenshot.sh
+```
+
+Or bind it to a keyboard shortcut in your desktop environment for easy access.
+
+## Screenshots
+<div align="center">
+    <img src="./previews/1749061714_grim.png" />
+</div>
---
+++ screenshot.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+dir="$HOME/.config/rofi"
+dir_screenshot="$HOME/Pictures/Screenshots/"
+theme="screenshot"
+
+host=$(uname -n)
+
+option_0="󰇄  Capture Desktop"
+option_1="󰹑  Capture Area"
+option_2="  Capture current window"
+option_3="  Capture in 5s"
+option_4="  Record Desktop"
+option_5="󱣴  Record Area"
+option_6="󰓛  Stop Recording"
+
+rofi_cmd() {
+    rofi -dmenu \
+        -p "$USER@$host" \
+        -mesg "  $dir_screenshot" \
+        -theme ${dir}/${theme}.rasi
+}
+
+run_rofi() {
+    echo -e "$option_0\n$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6" | rofi_cmd
+}
+
+run_cmd() {
+    if [[ $1 == '--option_0' ]]; then
+        sleep 0.3 && grim $(xdg-user-dir)/Pictures/Screenshots/$(date +'%s_grim.png')
+        notify-send -i camera -t 1000 "Screenshot saved" "Location: ${dir_screenshot}"
+    elif [[ $1 == '--option_1' ]]; then
+        sleep 0.3 && grim -g "$(slurp)" $(xdg-user-dir)/Pictures/Screenshots/$(date +'%s_grim.png')
+        notify-send -i camera -t 1000 "Area screenshot saved" "Location: ${dir_screenshot}"
+    elif [[ $1 == '--option_2' ]]; then
+        coords=$(hyprctl activewindow -j | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
+        sleep 0.3 && grim -g "$coords" $(xdg-user-dir)/Pictures/Screenshots/$(date +'%s_grim.png')
+        notify-send -i camera -t 1000 "Window screenshot saved" "Location: ${dir_screenshot}"
+    elif [[ $1 == '--option_3' ]]; then
+        sleep 5 && grim $(xdg-user-dir)/Pictures/Screenshots/$(date +'%s_grim.png')
+        notify-send -i camera -t 1000 "Delayed screenshot saved" "Location: ${dir_screenshot}"
+    elif [[ $1 == '--option_4' ]]; then
+        notify-send -i media-record -t 1000 "Recording started" "Saving to Videos folder"
+        sleep 1.2 && wf-recorder -f "$HOME/Videos/recording_$(date +%Y-%m-%d_%H-%M-%S).mp4" -b 5000k -r 30 &
+    elif [[ $1 == '--option_5' ]]; then
+        notify-send -i media-record -t 1000 "Area recording started" "Saving to Videos folder"
+        sleep 0.3 && wf-recorder -g "$(slurp)" -f "$HOME/Videos/recording_$(date +%Y-%m-%d_%H-%M-%S).mp4" -b 5000k -r 30 &
+    elif [[ $1 == '--option_6' ]]; then
+        if pgrep wf-recorder >/dev/null; then
+            pkill wf-recorder
+            notify-send -i media-playback-stop -t 1000 "Recording stopped"
+        else
+            notify-send -i dialog-warning -t 1000 "No recording in progress"
+        fi
+    else
+        exit 0
+    fi
+}
+
+chosen="$(run_rofi)"
+case ${chosen} in
+$option_0)
+    run_cmd --option_0
+    ;;
+$option_1)
+    run_cmd --option_1
+    ;;
+$option_2)
+    run_cmd --option_2
+    ;;
+$option_3)
+    run_cmd --option_3
+    ;;
+$option_4)
+    run_cmd --option_4
+    ;;
+$option_5)
+    run_cmd --option_5
+    ;;
+$option_6)
+    run_cmd --option_6
+    ;;
+esac
